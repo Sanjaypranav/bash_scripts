fee9144ecb742ac628fe7c2257a3a451c458b348
Sanjaypranav <sanjaypriya195@gmail.com>
2023-10-17 11:42:22 +0530

    updated setup for win and bash

---
+++ setup.bat
@@ -0,0 +1,3 @@
+set OPENAI_API_KEY="sk-<your key here>"
+set PINECONE_API_KEY="<your key here>"
+set PINECONE_API_ENV="us-west1-gcp"
---
+++ setup.sh
@@ -0,0 +1,5 @@
+export OPENAI_API_KEY="sk-<your key here>"
+export PINECONE_API_KEY="<your key here>"
+export PINECONE_API_ENV="us-west1-gcp"
+
+# Path: setup.sh
